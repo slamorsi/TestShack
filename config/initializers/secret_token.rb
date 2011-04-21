a17e3b65f727b56d3a9d1a786253ed29693ebba7
Islam Morsi <slamorsi@gmail.com>
2011-04-21 14:10:47 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testshack::Application.config.secret_token = '39d0f1e31b159d047ff287c480999412419902c9fc16edec826ea661a6ed7e248fa53fc38559b2fe68426c4ab7091fb6cfea22e7f68ca532eafb558c07567d42'
